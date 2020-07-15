2be81166f55bb7caa161eb876866177602bdefde
Diegoni <diego_nieto_1@hotmail.com>
2020-07-14 22:28:21 -0300

    Stock - Movimientos

--- application/views/stock/insert.php
+++ application/views/stock/insert.php
@@ -4,10 +4,19 @@
 		<div class="panel-heading">Movimientos Stock</div>
 		<div class="panel-body">
 			<div class="row">
-				<div class="col-md-8">
+				<div class="col-md-6">
 					<div class="form-group">
 						<label class="col-sm-2 control-label">Articulo:</label>
 						<input class="form-control" type='text' placeholder="Cod o Detalle" name='articulo' value='' id='articulo'/>
+					</div>
+				</div>
+				<div class="col-md-2">
+					<div class="form-group">
+						<label class="col-sm-2 control-label">Tipo:</label>
+						<select class="form-control" name='tipo' id='tipo'/>
+							<option value="1">Movimiento</option>
+							<option value="2">Total</option>
+						</select>
 					</div>
 				</div>
 				<div class="col-md-2">
@@ -52,10 +61,31 @@
 			</div>
 			<hr>
 			<div id="reglon_factura" class="row">
-					<span class="titulo_item_reglon col-sm-8"><b>DETALLE</b></span>
-					<span class="titulo_cant_item_reglon col-sm-2"><b>CANT</b></span>
-					<span class="titulo_px_item_reglon col-sm-2"><b>P.U </b></span>
-					<span class="col-sm-2"></span>
+				<div class="col-md-7">
+					<div class="form-group">
+						<label class="col-sm-2 control-label">Detalle</label>
+					</div>
+				</div>
+				<div class="col-md-1">
+					<div class="form-group">
+						<label class="col-sm-2 control-label">Stock Actual</label>
+					</div>
+				</div>
+				<div class="col-md-1">
+					<div class="form-group">
+						<label class="col-sm-2 control-label">Movimiento</label>
+					</div>
+				</div>
+				<div class="col-md-1">
+					<div class="form-group">
+						<label class="col-sm-2 control-label">Stock Nuevo</label>
+					</div>
+				</div>
+				<div class="col-md-2">
+					<div class="form-group">
+						<label class="col-sm-2 control-label">Eliminar</label>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -91,11 +121,9 @@ $(function() {
 				$('#stock_deseado').val(stock_deseado);
 
 				if(pos!=-1){
-		  		nuevo=false;
-		  		//console.log("modifica:",este);
-		      cant_cargada=$('#cant_'+este).val();
-		      //console.log(cant_cargada);
-		      $('#cantidad').val(cant_cargada);
+		  		nuevo = false;
+		  		cant_cargada = $('#cant_'+este).val();
+					$('#cantidad').val(cant_cargada);
 					$('#cantidad').select();
 				}
 			} else {
@@ -110,41 +138,55 @@ $(function() {
 
 	$("#cantidad").keypress(function( event ) {
 		if ( event.which == 13 ) {
-			$("#carga_articulo").click();
-			$("#cantidad").attr('disabled','disabled');
+			cantidad = parseInt($("#cantidad").val());
+			if(Number.isInteger(cantidad)){
+				$("#carga_articulo").click();
+				$("#cantidad").attr('disabled','disabled');
+			} else {
+				$("#cantidad").val('');
+				alert('por favor ingrese cantidad');
+			}
 		}
 	});
 });
 
 function carga(elem) {
-	cantidad = $('#cantidad').val();
+	cantidad = parseFloat($('#cantidad').val());
+	tipo = parseFloat($('#tipo').val());
+	stock = parseFloat($('#stock').val());
 	if($("#articulo").val().length >= 1) {
 		este = elem.id;
 		if(nuevo){
 			items_reglon.push(este);
 		}
-		agrega_a_reglon(este,elem.value,cantidad,nuevo);
+		agrega_a_reglon(este, elem.value, cantidad, tipo, stock, nuevo);
 		reset_item();
 	} else {
 		reset_item();
 	}
 }
 
-function agrega_a_reglon(este,texto,cantidad,bandera) {
+function agrega_a_reglon(este, texto, cantidad, tipo, stock, bandera) {
 	if(bandera){
+		if (tipo == 2){
+			cantidad = cantidad - stock;
+		}
+		var newStock = stock + cantidad;
 		largo	= $('#reglon_factura').height();
-
 		largo	= largo + 30;
 		//console.log("largo despúes:"+largo);
 
 		$('#reglon_factura').height(largo);
 		$('#reglon_factura').append('<div class="row">');
 		$('#reglon_factura').append('<div id="cont_borra'+este+'" class="cont_reglon_item_presup row" style="padding-left: 15px"></div>');
-		$('#cont_borra'+este).append('<span class="item_reglon col-md-8" id='+este+' >'+texto+'</span>');
-		$('#cont_borra'+este).append('<input  disabled class="cant_item_reglon col-md-2" id=cant_'+este+' value='+cantidad+'>');
+		$('#cont_borra'+este).append('<span class="item_reglon col-md-7" id='+este+' >'+texto+'</span>');
+		$('#cont_borra'+este).append('<div class="col-md-1">'+stock+'</div>');
+		$('#cont_borra'+este).append('<input class="cant_item_reglon" id=cant_'+este+' value='+cantidad+' type="hidden">');
+		$('#cont_borra'+este).append('<div class="col-md-1">'+cantidad+'</div>');
+		$('#cont_borra'+este).append('<div class="col-md-1">'+newStock+'</div>');
 		$('#cont_borra'+este).append('<div class="col-md-2" id=cont_botones'+este+'></div>');
 		$('#cont_botones'+este).append('<button title="Borrar linea" class="ico_borra btn btn-danger btn-xs pull-left" onclick="borra_reglon('+este+')" id="ico_borra'+este+'"></button>');
-		$('#reglon_factura').append('</div>');
+		$('#reglon_factura').append('</div><hr class="hrLine">');
 		$('#ico_borra'+este).append('<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>');
 
 		reset_item();
@@ -164,19 +206,6 @@ function reset_item() {
     $("#articulo").focus();
 }
 
-
-
-function limpia_cli() {
-	$("#carga_cliente").prop( "disabled", false).val('').focus();
-	$('#apellido_cliente').val('');
-	$('#nombre_cliente').val('');
-	$('#domicilio_cliente').val('');
-	$('#cuit_cliente').val('');
-	$('#id_cliente').val(0);
-	$('#forma_pago').val(CONTADO);
-	$('#tipo_comprobante').val(COMPROBANTE_PRESUPUESTO);
-	$("#cont_boton").text('CARGAR PRESUPUESTO');
-}
 
 
 /*---------------------------------------------------------------------------------
@@ -234,10 +263,15 @@ function borra_reglon(a) {
 
 	$('#cont_borra'+a).empty();
 	$('#cont_borra'+a).remove();
-	var nuevo_largo=$('#reglon_factura').height();
-	nuevo_largo=nuevo_largo-30;
+	var nuevo_largo = $('#reglon_factura').height();
+	nuevo_largo = nuevo_largo - 30;
 	$('#reglon_factura').height(nuevo_largo);
 }
 
 
 </script>
+<style>
+.hrLine{
+	margin-top: 10px;
+	margin-bottom: 10px;
+}
